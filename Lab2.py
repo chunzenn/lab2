d943242a3c42758cd7060e58d381de91e4eb85eb
chunzen <chunzen.23@ichat.s.edu.sg>
2024-04-29 15:09:17 +0800

    Return value

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,17 @@
-print("ET0735 (DevOps for AIoT) - Lab_2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    num = input("Enter numbers")
+
+def calc_average():
+    print("calc_average")
+
+def find_min_max():
+    print("find_min_max")
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
